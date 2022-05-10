cb28115d68f50fa3393b9904d5592e2e59e0fffc
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2022-05-10 23:36:39 +0530

    implement stepphone

--- frontend/src/pages/Steps/StepPhoneEmail/StepPhoneEmail.jsx
+++ frontend/src/pages/Steps/StepPhoneEmail/StepPhoneEmail.jsx
@@ -1,12 +1,36 @@
-import React from 'react'
-
+import React, {useState} from 'react';
+import Phone from './Phone/Phone';
+import Email from './Email/Email';
+import styles from './StepEmail.module.css'
+const phoneEmailMap = {
+    phone: Phone,
+    email : Email
+}
 const StepPhoneEmail = ({onNext}) => {
-  return (
-    <>
-        <div>StepPhoneEmail</div>
-        <button onClick={onNext}>Next</button>
-    </>
-  )
+    const [type, setType]  = useState('phone');
+    const Component = phoneEmailMap[type];
+    function onNext (){
+      setType(type+1);
+    }
+    return (
+        <>
+        <div className={styles.cardWrapper}>
+            <div>
+                <div className={styles.buttonWrap}>
+                    <button className={`${styles.tabButton} ${type==='phone' ? styles.active : styles.deactive} ` } onClick={()=> setType('phone')}>
+                        <img  src="/images/Smartphone.png" alt="" />
+                    </button>
+                    <button className={`${styles.tabButton} ${type==='email' ? styles.active : styles.deactive} `} onClick={()=> setType('email')}>
+                        <img  src="/images/Email.png" alt="" />
+                    </button>
+                </div>
+                <Component onNext = {onNext}/>
+            </div>
+        </div>
+        
+        
+        </>
+    )
 }
 
 export default StepPhoneEmail
