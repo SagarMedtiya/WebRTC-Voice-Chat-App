646093cf29e2ff0fcebaef4507a6076739b5d144
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2022-05-10 23:33:41 +0530

    implement routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,70 @@
 import './App.css';
-import {  BrowserRouter, Routes, Route} from 'react-router-dom';
-import Home from './pages/Home/Home'
-import Navigation from './components/shared/Navi/Navigation'
-import Register from './pages/Register/Register'
-import Login from './pages/Login/Login'
-function App() {
-  return (
-    <BrowserRouter>
-    <Navigation/>
-      <Routes>
-        <Route path='/' exact element={<Home/>}/>
-      </Routes>
-      <Routes>
-        <Route path='/register' exact element={<Register/>}/>
-      </Routes>
-      <Routes>
-        <Route path='/login' exact element={<Login/>}/>
-      </Routes>
-    </BrowserRouter>
-   
-  );
-}
+import {  BrowserRouter, Routes, Route,Navigate, useLocation} from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Navigation from './components/shared/Navi/Navigation';
+import Activate from './pages/Activate/Activate';
+import Authenticate from './pages/Authenticate/Authenticate';
+import Rooms from './pages/Rooms/Rooms';
+const isAuth = false;
+const user ={
+    activated: true,
+};
 
+function App() {
+    return (
+        <BrowserRouter>
+            <Navigation/>
+            <Routes>
+                <Route path='/' exact element={
+                    <GuestRoute>
+                        <Home/>
+                    </GuestRoute>}/>
+                <Route path="authenticate" element=
+                    {<GuestRoute>
+                        <Authenticate/>
+                    </GuestRoute>}/>
+                <Route path="activate" element={
+                    <SemiProtectedRoute>
+                        <Activate/>
+                    </SemiProtectedRoute>
+                }/>
+                <Route path="rooms" element={
+                    <ProtectedRoute>
+                        <Rooms/>
+                    </ProtectedRoute>
+                }/>
+            </Routes>
+        </BrowserRouter>
+  );
+};
+
+const GuestRoute = ({children})=>{
+    const location = useLocation();
+    if (isAuth) {
+        return <Navigate to="/rooms" state={{ from: location }} />
+    }
+    return children;
+};
+
+const SemiProtectedRoute = ({children})=>{
+    const location = useLocation();
+    if (!isAuth) {
+        return <Navigate to="/" state={{ from: location }} />;
+    };
+    if(isAuth && !user.activated){
+        return children
+    };
+    return <Navigate to="/rooms" state={{ from: location }}/>;
+};
+
+const ProtectedRoute = ({children})=>{
+    const location = useLocation();
+    if (!isAuth) {
+        return <Navigate to="/" state={{ from: location }} />;
+    };
+    if(isAuth && !user.activated){
+        return <Navigate to="/activate" state={{ from: location }}/>
+    };
+    return children;
+};
 export default App;
