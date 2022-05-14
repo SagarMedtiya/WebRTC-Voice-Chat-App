f3ecdba6e51bd38d16cd18a67b4ffb12a04035b5
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2022-05-14 23:47:35 +0530

    get the detail of phone number

--- frontend/src/pages/Steps/StepPhoneEmail/Phone/Phone.jsx
+++ frontend/src/pages/Steps/StepPhoneEmail/Phone/Phone.jsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Card from '../../../../components/shared/Card/Card';
 import Button from '../../../../components/shared/Button/Button';
-const Phone = () => {
+import TextInput from '../../../../components/shared/TextInput/TextInput';
+import styles from '../StepEmail.module.css'
+const Phone = ({onNext}) => {
+  const [phoneNumber, setPhoneNumber] = useState('');
   return (
     <Card title="Enter your phone number" icon="Smartphone">
-            <div>
-                <Button text="Next"/>
-            </div>
+        <div>
+            <TextInput value={phoneNumber} onChange={(e)=>setPhoneNumber(e.target.value)}/>
+        </div>
+        <div className={styles.actionButtonWrap}>
+            <Button onClick={onNext} text="Next"/>
+        </div>
+            <p className={styles.para}>By entering your number, you are agreeing to out Terms of Service and Privacy Policy. Thanks!</p>
             
-        </Card>
+    </Card>
   )
 }
 
