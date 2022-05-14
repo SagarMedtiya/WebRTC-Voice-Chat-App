695d36bc30c9dc2da4a200917b4a0913989f2912
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2022-05-14 23:50:10 +0530

    otp frontend file

--- frontend/src/pages/Steps/StepOtp/StepOtp.jsx
+++ frontend/src/pages/Steps/StepOtp/StepOtp.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-
+import React,{useState} from 'react'
+import Card from '../../../components/shared/Card/Card';
+import Button from '../../../components/shared/Button/Button';
+import TextInput from '../../../components/shared/TextInput/TextInput';
+import styles from './StepOtp.module.css'
 const StepOtp = ({onNext}) => {
+    const [otp,setOtp] = useState('');
   return (
     <>
-        <div>StepOtp</div>
-        <button onClick={onNext}>Next</button>
+        <div className={styles.cardWrapper}>
+            <Card title="Enter the otp" icon="Lock">
+                <div>
+                    <TextInput value={otp} onChange={(e)=>setOtp(e.target.value)}/>
+                </div>
+                <div className={styles.actionButtonWrap}>
+                    <Button onClick={onNext} text="Next"/>
+                </div>
+                    <p className={styles.para}>By entering your number, you are agreeing to out Terms of Service and Privacy Policy. Thanks!</p>
+                
+            </Card>
+        </div>
+       
     </>
   )
 }
