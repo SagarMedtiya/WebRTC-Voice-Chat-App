85967f7ba4b74f7c27cef2139a8ebd1787a24204
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2022-05-14 23:52:03 +0530

    set the email

--- frontend/src/pages/Steps/StepPhoneEmail/Email/Email.jsx
+++ frontend/src/pages/Steps/StepPhoneEmail/Email/Email.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
-
-const Email = () => {
+import React,{useState} from 'react'
+import Card from '../../../../components/shared/Card/Card';
+import Button from '../../../../components/shared/Button/Button';
+import TextInput from '../../../../components/shared/TextInput/TextInput';
+import styles from '../StepEmail.module.css';
+const Email = ({onNext}) => {
+    const [email, setemail] = useState('');
   return (
-    <div>Email</div>
+    <Card title="Enter your email id" icon="Emoji">
+            <div>
+                <TextInput value={email} onChange={(e)=>setemail(e.target.value)}/>
+            </div>
+            <div className={styles.actionButtonWrap}>
+                <Button onClick={onNext} text="Next"/>
+            </div>
+            <p className={styles.para}>By entering your number, you are agreeing to out Terms of Service and Privacy Policy. Thanks!</p>
+        </Card>
   )
 }
 
