0d7b84464a74cf636505a9840ecf5ef11ff0ac5e
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2022-05-14 23:51:45 +0530

    hashing phone and email file

--- frontend/src/pages/Steps/StepPhoneEmail/StepPhoneEmail.jsx
+++ frontend/src/pages/Steps/StepPhoneEmail/StepPhoneEmail.jsx
@@ -9,9 +9,6 @@ const phoneEmailMap = {
 const StepPhoneEmail = ({onNext}) => {
     const [type, setType]  = useState('phone');
     const Component = phoneEmailMap[type];
-    function onNext (){
-      setType(type+1);
-    }
     return (
         <>
         <div className={styles.cardWrapper}>
