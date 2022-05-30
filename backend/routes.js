375ab30e94f29b360380d3ec576897bec5c61f4c
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2022-05-30 13:41:37 +0530

    add verify-otp routes

--- backend/routes.js
+++ backend/routes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const authcontroller  =require('./controllers/auth-controller')
 
 router.post('/api/send-otp',authcontroller.sendOtp);
+router.post('/api/verify-otp',authcontroller.verifyOtp)
 
 
 module.exports = router;
