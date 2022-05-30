add12214dca3607998608b48a81ec9f90f5e5a43
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2022-05-30 13:27:24 +0530

    configure otp services

---
+++ backend/services/otp-service.js
@@ -0,0 +1,14 @@
+const crypto = require('crypto')
+
+const { sendOtp } = require("../controllers/auth-controller")
+
+class otpService(){
+    generateOtp(){
+        const otp = crypto 
+    }
+    sendBysms(){}
+    verifyOtp(){}
+}
+
+
+module.exports = new otpService
