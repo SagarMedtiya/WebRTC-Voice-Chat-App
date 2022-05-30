6b6afbdfee68af942934a15e8e6ddbec3dd44a73
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2022-05-30 13:28:03 +0530

    database connection

---
+++ backend/database.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+function Dbconnect(){
+
+    const url = process.env.MONGODB;
+    mongoose.connect(url,{ useNewUrlParser: true, useUnifiedTopology: true
+        }).then(()=>{
+            console.log('Connection Successful');
+        }).catch((error)=>{     
+            console.log('Something went wrong', error)
+        });
+}
