9ff35067c4ded65d046bb3c4db120e66fa30d4ca
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2022-06-03 23:39:47 +0530

    add submit method

--- frontend/src/pages/Steps/StepPhoneEmail/Phone/Phone.jsx
+++ frontend/src/pages/Steps/StepPhoneEmail/Phone/Phone.jsx
@@ -2,16 +2,24 @@ import React,{useState} from 'react';
 import Card from '../../../../components/shared/Card/Card';
 import Button from '../../../../components/shared/Button/Button';
 import TextInput from '../../../../components/shared/TextInput/TextInput';
-import styles from '../StepEmail.module.css'
+import styles from '../StepEmail.module.css';
+import { sendOtp } from '../../../../http/index'
 const Phone = ({onNext}) => {
   const [phoneNumber, setPhoneNumber] = useState('');
+    async function submit(){
+        //server request
+        const res = await sendOtp({ phone: phoneNumber });
+        console.log(res);
+        //onNext();
+    }
+
   return (
     <Card title="Enter your phone number" icon="Smartphone">
         <div>
             <TextInput value={phoneNumber} onChange={(e)=>setPhoneNumber(e.target.value)}/>
         </div>
         <div className={styles.actionButtonWrap}>
-            <Button onClick={onNext} text="Next"/>
+            <Button onClick={submit} text="Next"/>
         </div>
             <p className={styles.para}>By entering your number, you are agreeing to out Terms of Service and Privacy Policy. Thanks!</p>
             
