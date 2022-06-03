7f33ae315fbfa48a6a5ce930c9c9499e39ed3bb7
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2022-06-03 23:35:42 +0530

    add cors functionality

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,14 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 4000;
 const router = require('./routes');
+const cors = require('cors')
 const Dbconnect = require('./database')
 
 Dbconnect();
+const corsOption ={
+    origin : ['http://localhost:3000']
+}
+app.use(cors(corsOption))
 app.use(express.json());
 app.use(router);
 app.get('/',(req,res)=>{
