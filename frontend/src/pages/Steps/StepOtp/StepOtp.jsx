4edd0d1644bc230c2e15d255088a69e825ee0f1a
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2022-06-04 22:15:21 +0530

    stepotp configuration

--- frontend/src/pages/Steps/StepOtp/StepOtp.jsx
+++ frontend/src/pages/Steps/StepOtp/StepOtp.jsx
@@ -2,9 +2,26 @@ import React,{useState} from 'react'
 import Card from '../../../components/shared/Card/Card';
 import Button from '../../../components/shared/Button/Button';
 import TextInput from '../../../components/shared/TextInput/TextInput';
-import styles from './StepOtp.module.css'
-const StepOtp = ({onNext}) => {
+import styles from './StepOtp.module.css';
+import { verifyOtp } from '../../../http';
+import { useSelector, useDispatch } from 'react-redux';
+import { setAuth } from '../../../store/authSlice'
+
+const StepOtp = () => {
     const [otp,setOtp] = useState('');
+    const dispatch = useDispatch()
+    const {phone, hash } = useSelector((state)=> state.auth.otp)
+    async function submit(){
+        try{
+            const {data} = await verifyOtp({ otp, phone,hash});
+            dispatch(setAuth(data))
+            console.log(data); 
+        }catch(err){
+            console.log(err);
+        }
+        
+    
+    }
   return (
     <>
         <div className={styles.cardWrapper}>
@@ -13,7 +30,7 @@ const StepOtp = ({onNext}) => {
                     <TextInput value={otp} onChange={(e)=>setOtp(e.target.value)}/>
                 </div>
                 <div className={styles.actionButtonWrap}>
-                    <Button onClick={onNext} text="Next"/>
+                    <Button onClick={submit} text="Next"/>
                 </div>
                     <p className={styles.para}>By entering your number, you are agreeing to out Terms of Service and Privacy Policy. Thanks!</p>
                 
