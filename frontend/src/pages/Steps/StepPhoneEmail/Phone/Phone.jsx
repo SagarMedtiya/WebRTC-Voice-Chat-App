a988f3cd5053d960bb8a9c75a7a9d097cb9c99c5
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2022-06-04 22:15:43 +0530

    add useSelector hook

--- frontend/src/pages/Steps/StepPhoneEmail/Phone/Phone.jsx
+++ frontend/src/pages/Steps/StepPhoneEmail/Phone/Phone.jsx
@@ -3,14 +3,20 @@ import Card from '../../../../components/shared/Card/Card';
 import Button from '../../../../components/shared/Button/Button';
 import TextInput from '../../../../components/shared/TextInput/TextInput';
 import styles from '../StepEmail.module.css';
-import { sendOtp } from '../../../../http/index'
+import { sendOtp } from '../../../../http/index';
+import { useDispatch } from 'react-redux';
+import { setOtp } from '../../../../store/authSlice';
+ 
 const Phone = ({onNext}) => {
-  const [phoneNumber, setPhoneNumber] = useState('');
+    const [phoneNumber, setPhoneNumber] = useState('');
+    const dispatch = useDispatch();
+
     async function submit(){
         //server request
-        const res = await sendOtp({ phone: phoneNumber });
-        console.log(res);
-        //onNext();
+        const {data} = await sendOtp({ phone: phoneNumber });
+        console.log(data)
+        dispatch(setOtp({ phone: data.phone, hash: data.hash}))
+        onNext();
     }
 
   return (
