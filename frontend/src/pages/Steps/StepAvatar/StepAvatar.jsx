783ef41d3e94c651cb424f7d02568c5f74b6ebcd
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2022-06-06 21:30:16 +0530

    implement upload avatar component

--- frontend/src/pages/Steps/StepAvatar/StepAvatar.jsx
+++ frontend/src/pages/Steps/StepAvatar/StepAvatar.jsx
@@ -1,12 +1,32 @@
-import React from 'react'
-
+import React,{useState} from 'react';
+import styles from './StepAvatar.module.css';
+import TextInput from '../../../components/shared/TextInput/TextInput';
+import Card from '../../../components/shared/Card/Card';
+import Button from '../../../components/shared/Button/Button';
+import { useSelector, useDispatch } from 'react-redux';
+import { setName, setAvatar } from '../../../store/activationSlice';
 const StepAvatar = ({onNext}) => {
-  return (
-    <>
-        <div>StepAvatar</div>
-        <button onClick={onNext}>Next</button>
-    </>
-  )
+    const {name} = useSelector((state)=> state.activate);
+    const [image, setImage] = useState('/images/mokey-avatar.jpg')
+    function submit(){
+
+    }
+    return (
+        <>
+            <div className={styles.cardWrapper}>
+                    <Card title={`Okay, ${name}`} icon="Monkey">
+                        <p className={styles.para}>How's this photo?</p>
+                        <div className={styles.avatarWrapper}>
+                            <img src={image} alt="avatar" />    
+                        </div>   
+                        <div className={styles.actionButtonWrap}>
+                            <Button onClick={submit} text="Next"/>
+                        </div>
+                            
+                    </Card>
+                </div>
+        </>
+    )
 }
 
 export default StepAvatar
