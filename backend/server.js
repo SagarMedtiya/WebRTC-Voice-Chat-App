434ffc34ec4f04b73dd30bd6538324d4aeee2cd3
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2022-06-07 22:03:42 +0530

    add cookie-parser module

--- backend/server.js
+++ backend/server.js
@@ -5,14 +5,16 @@ const PORT = process.env.PORT || 4000;
 const router = require('./routes');
 const cors = require('cors')
 const Dbconnect = require('./database')
+const cookieParser = require('cookie-parser')
 
+app.use(cookieParser());
 Dbconnect();
 const corsOption ={
     origin : ['http://localhost:3000'],
     credentials:true,
 }
-app.use(cors(corsOption))
-app.use(express.json());
+app.use(cors(corsOption));
+app.use(express.json({limit: '8mb'}));
 app.use(router);
 app.get('/',(req,res)=>{
     res.send('hello from express');
