edee45cbeaae297fe2a40a218311e71c0ff6e7c4
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2022-06-07 22:05:06 +0530

    add reader web browser api

--- frontend/src/pages/Steps/StepAvatar/StepAvatar.jsx
+++ frontend/src/pages/Steps/StepAvatar/StepAvatar.jsx
@@ -5,11 +5,29 @@ import Card from '../../../components/shared/Card/Card';
 import Button from '../../../components/shared/Button/Button';
 import { useSelector, useDispatch } from 'react-redux';
 import { setName, setAvatar } from '../../../store/activationSlice';
-const StepAvatar = ({onNext}) => {
-    const {name} = useSelector((state)=> state.activate);
-    const [image, setImage] = useState('/images/mokey-avatar.jpg')
-    function submit(){
+import { activate } from '../../../http/index';
 
+
+const StepAvatar = ({onNext}) => {
+    const {name, avatar} = useSelector((state)=> state.activate);
+    const dispatch = useDispatch();
+    const [image, setImage] = useState('/images/monkey.jpg')
+    function captureImage(e){
+       const file = e.target.files[0];
+       const reader = new FileReader();
+       reader.readAsDataURL(file)
+       reader.onloadend = function(){
+            setImage(reader.result);
+            dispatch(setAvatar(reader.result))
+       }
+    }
+    async function submit(){
+        try{
+            const {data} = await activate({name, avatar});
+            console.log(data);
+        }catch(err){
+            console.log(err.message);
+        }
     }
     return (
         <>
@@ -17,8 +35,12 @@ const StepAvatar = ({onNext}) => {
                     <Card title={`Okay, ${name}`} icon="Monkey">
                         <p className={styles.para}>How's this photo?</p>
                         <div className={styles.avatarWrapper}>
-                            <img src={image} alt="avatar" />    
-                        </div>   
+                            <img className={styles.avatarImage} src={image} alt="avatar" />    
+                        </div>
+                        <div>
+                            <input onChange={captureImage} id="avatarInput" type="file"  className={styles.avatarInput}/>
+                            <label className={styles.avatarLabel} htmlFor="avatarInput">Choose a different pic</label>
+                        </div>  
                         <div className={styles.actionButtonWrap}>
                             <Button onClick={submit} text="Next"/>
                         </div>
