75a18cfc9a639eee5e73c230d6e8666d4ab203d6
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2022-06-07 22:06:22 +0530

    add auth middleware and activation controller

--- backend/routes.js
+++ backend/routes.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const authcontroller  =require('./controllers/auth-controller')
-
+const activateController = require('./controllers/activate-controller');
+const authMiddleware = require('./middleware/auth-middleware')
 router.post('/api/send-otp',authcontroller.sendOtp);
 router.post('/api/verify-otp',authcontroller.verifyOtp)
-
+router.post('/api/activate',authMiddleware, activateController.activate)
 
 module.exports = router;
