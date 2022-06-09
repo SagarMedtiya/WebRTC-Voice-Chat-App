db5ddf34f489e1b2d1d64d9f2ff1123dcbfd4729
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2022-06-09 08:43:50 +0530

    configure storage folder

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,7 @@ const corsOption ={
     credentials:true,
 }
 app.use(cors(corsOption));
+app.use('/storage',express.static('storage'))
 app.use(express.json({limit: '8mb'}));
 app.use(router);
 app.get('/',(req,res)=>{
