537dde0591b180967fcb4aeeecddb323d1e27638
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2022-06-09 23:50:08 +0530

    add refresh route

--- backend/routes.js
+++ backend/routes.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const authcontroller  =require('./controllers/auth-controller')
 const activateController = require('./controllers/activate-controller');
-const authMiddleware = require('./middleware/auth-middleware')
+const authMiddleware = require('./middleware/auth-middleware');
+const authController = require('./controllers/auth-controller');
 router.post('/api/send-otp',authcontroller.sendOtp);
 router.post('/api/verify-otp',authcontroller.verifyOtp)
 router.post('/api/activate',authMiddleware, activateController.activate)
+router.post('/api/refresh',authController.refresh)
 
 module.exports = router;
