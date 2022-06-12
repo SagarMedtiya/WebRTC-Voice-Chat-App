93a5acbc369431e9a408d36c6680b7c17ab84c93
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2022-06-12 20:36:38 +0530

    verify and find refreshToken to get accessToken

--- backend/services/token-service.js
+++ backend/services/token-service.js
@@ -3,7 +3,7 @@ const refreshModel = require('../models/refresh-model');
 class TokenService{
     generateTokens(payload){
         const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_TOKEN_SECRET,{
-            expiresIn: '1h'
+            expiresIn: '1m'
         }) 
         const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_TOKEN_SECRET,{
             expiresIn: '1y'
@@ -23,7 +23,21 @@ class TokenService{
     async verifyAccessToken(token){
         return jwt.verify(token, process.env.JWT_ACCESS_TOKEN_SECRET);
     }
-
+    async verifyRefreshToken(token){
+        return jwt.verify(token,process.env.JWT_ACCESS_TOKEN_SECRET);
+    }
+    async findRefreshToken(userId,token){
+        return await refreshModel.findOne({
+            _id :userId,
+            token:token
+        })
+    }
+    async updateRefreshToken(userId,token){
+        return await refreshModel.updateOne({
+            _id :userId,
+            token:token
+        })
+    }
 }
 
 module.exports = new TokenService()
