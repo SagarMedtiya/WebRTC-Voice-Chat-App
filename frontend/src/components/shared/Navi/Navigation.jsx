c33cbecb8de1ebc9423ba2068fd0db58148a0e7d
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2022-06-16 21:59:38 +0530

    add logout method

--- frontend/src/components/shared/Navi/Navigation.jsx
+++ frontend/src/components/shared/Navi/Navigation.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import style from './Navigation.module.css'
-const navigation = () => {
+import { logout } from '../../../http';
+import style from './Navigation.module.css';
+import { useDispatch,useSelector } from 'react-redux';
+import { setAuth } from '../../../store/authSlice';
+const Navigation = () => {
   const brandStyle ={
     color: '#fff',
     textDecoration: 'none',
@@ -13,15 +16,25 @@ const navigation = () => {
   const logoText={
     marginLeft: '10px',
   }
-
+  const dispatch = useDispatch();
+  const {isAuth} = useSelector(state=>state.auth)
+  async function logoutUser(){
+    try{
+        const {data} = await logout();
+        dispatch(setAuth(data));
+    }catch(err){
+        console.log(err)
+    }
+  }
   return (
     <nav className={`${style.navbar} container`}>
       <Link style={brandStyle} to='/'>
         <img  src="/images/Logo.png" alt="" />
         <span style={logoText}>CodersVilla</span>
       </Link>
+        {isAuth && <button onClick={logoutUser}>Logout</button>}
     </nav>
   )
 }
 
-export default navigation
+export default Navigation
