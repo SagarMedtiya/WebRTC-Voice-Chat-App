0b38229728ca3e23a9bc5cacef5092139e2c418d
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2022-06-16 21:58:38 +0530

    add logout route

--- frontend/src/http/index.js
+++ frontend/src/http/index.js
@@ -14,7 +14,8 @@ const api = axios.create({
 //list of all the end points
 export const sendOtp = (data)=>api.post('/api/send-otp',data);
 export const verifyOtp = (data)=>api.post('/api/verify-otp', data)
-export const activate = (data)=>api.post('/api/activate', data) 
+export const activate = (data)=>api.post('/api/activate', data);
+export const logout = ()=>api.post('/api/logout') 
 
 //interceptors
 api.interceptors.response.use(
