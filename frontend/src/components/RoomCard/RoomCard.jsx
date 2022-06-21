e847502c3851fdf6b746600a5a7dc86a64e54fc0
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2022-06-21 16:51:52 +0530

    create room component

--- frontend/src/components/RoomCard/RoomCard.jsx
+++ frontend/src/components/RoomCard/RoomCard.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
-import styles from ''
+import styles from './RoomCard.module.css'
 const RoomCard = ({room}) => {
   return (
-    <div className={styles.}></div>
+    <div className={styles.card}>
+        <h3 className={styles.topic}>{room.topic}</h3>
+        <div className={styles.speaker}>
+            <div className={styles.left}>
+                {room.speakers.map(speaker=>(
+                    <img src={speaker.avatar} alt="" />
+                ))}
+            </div>
+            <div className={styles.right}>
+                {room.speakers.map(speaker=>(
+                    <div className={styles.nameWrapper}>
+                        <span>{speaker.name}</span>
+                        <img src="/images/chat.png" alt="" />
+                    </div>
+                ))}
+            </div>
+        </div>
+        <div className={styles.peopleCount}>
+            <span>{room.totalPeople}</span>
+            <img src="/images/user.png" alt="" />
+        </div>
+    </div>
   )
 }
 
