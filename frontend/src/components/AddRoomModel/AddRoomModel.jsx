62725423f168323320b853863ea5911b04a67119
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2022-06-29 23:41:28 +0530

    room model

--- frontend/src/components/AddRoomModel/AddRoomModel.jsx
+++ frontend/src/components/AddRoomModel/AddRoomModel.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import styles from './AddRoomModel.module.css'
+import TextInput from '../shared/TextInput/TextInput'
 const AddRoomModel = () => {
   return (
     <div className={styles.modelMask}>
         <div className={styles.modelBody}>
-            <div className={styles.modelHeader}></div>
+            <div className={styles.modelHeader}>
+                <h3>Enter the topic to be discussed</h3>
+                <TextInput/>
+            </div>
             <div className={styles.modelFooter}></div>
         </div>
     </div>
