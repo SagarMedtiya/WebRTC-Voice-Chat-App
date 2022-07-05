17fdd59c34d741b212701e803ea78c541b25e362
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2022-07-05 23:41:52 +0530

    implement the style

--- frontend/src/components/shared/TextInput/TextInput.jsx
+++ frontend/src/components/shared/TextInput/TextInput.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import styles from './TextInput.module.css'
 const TextInput = (props) => {
   return (
-    <input className={styles.input} type="text" {...props} />
+    <input className={styles.input} style={{width: props.fullwidth === 'true'? '100%' :'inherit'}} type="text" {...props} />
   )
 }
 
