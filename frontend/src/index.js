af4aa1979987b1dc0fc19f458da7677800b4b5a2
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2022-07-05 23:40:46 +0530

    fix the bugs

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 import {store} from './store/index';
 import { Provider } from 'react-redux'
 
