a769cca902c5fb091220d2c8ec2077a6f9e7ee7f
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2022-07-05 23:43:41 +0530

    add room modal file

--- frontend/src/components/AddRoomModel/AddRoomModel.jsx
+++ frontend/src/components/AddRoomModel/AddRoomModel.jsx
@@ -5,11 +5,30 @@ const AddRoomModel = () => {
   return (
     <div className={styles.modelMask}>
         <div className={styles.modelBody}>
+            <button><img src="/images/close.png" alt="" /></button>
             <div className={styles.modelHeader}>
-                <h3>Enter the topic to be discussed</h3>
-                <TextInput/>
+                <h3 className={styles.heading}>Enter the topic to be discussed</h3>
+                <TextInput fullwidth="true"/>
+                <h3 className={styles.roomHeading}>Room Types</h3>
+                <div className={styles.roomTypes}>
+                    <div className={styles.typeBox}>
+                        <img src="/images/open.png" alt="" />
+                        <span>Open</span>
+                    </div>
+                    <div className={styles.typeBox}>
+                        <img src="/images/social.png" alt="" />
+                        <span>Social</span>
+                    </div>
+                    <div className={styles.typeBox}>
+                        <img src="/images/lock1.png" alt="" />
+                        <span>Private</span>
+                    </div>
+                </div>
             </div>
-            <div className={styles.modelFooter}></div>
+            <div className={styles.modelFooter}>
+                <h3>Start a room, open to everyone</h3>
+                <button className={styles.footerButton}><img src="/images/party.png" alt="" /><span>Let's Go!</span></button>
+            </div>
         </div>
     </div>
   )
