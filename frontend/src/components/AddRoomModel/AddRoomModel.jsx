93083f0d9aa87dd72dc4d6ffdf640e5ced6c0ab6
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2022-07-27 22:23:33 +0530

    topic and roomType data

--- frontend/src/components/AddRoomModel/AddRoomModel.jsx
+++ frontend/src/components/AddRoomModel/AddRoomModel.jsx
@@ -1,25 +1,38 @@
-import React from 'react'
+import React,{useState} from 'react'
 import styles from './AddRoomModel.module.css'
 import TextInput from '../shared/TextInput/TextInput'
-const AddRoomModel = () => {
+import {createRoom as create} from '../../http/index'
+
+const AddRoomModel = ({onClose}) => {
+    const [roomType, setRoomType] =useState('open');
+    const [topic, setTopic] = useState('')
+    async function createRoom (){
+        try{
+            if(!topic) return ;
+            const {data} = await create({topic, roomType});
+            console.log(data);
+        }catch(err){
+            console.log(err.message)
+        }
+    }
   return (
     <div className={styles.modelMask}>
         <div className={styles.modelBody}>
-            <button><img src="/images/close.png" alt="" /></button>
+            <button onClick={onClose} className={styles.closeButton}><img src="/images/close.png" alt="" /></button>
             <div className={styles.modelHeader}>
                 <h3 className={styles.heading}>Enter the topic to be discussed</h3>
-                <TextInput fullwidth="true"/>
+                <TextInput fullwidth="true" value={topic} onChange={(e)=>{setTopic(e.target.value)}}/>
                 <h3 className={styles.roomHeading}>Room Types</h3>
                 <div className={styles.roomTypes}>
-                    <div className={styles.typeBox}>
+                    <div onClick={()=>setRoomType('open')} className={`${styles.typeBox} ${roomType==='open' ? styles.active:''}`}>
                         <img src="/images/open.png" alt="" />
                         <span>Open</span>
                     </div>
-                    <div className={styles.typeBox}>
+                    <div onClick={()=>setRoomType('social')} className={`${styles.typeBox} ${roomType==='social' ? styles.active:''}`}>
                         <img src="/images/social.png" alt="" />
                         <span>Social</span>
                     </div>
-                    <div className={styles.typeBox}>
+                    <div onClick={()=>setRoomType('private')} className={`${styles.typeBox} ${roomType==='private' ? styles.active:''}`}>
                         <img src="/images/lock1.png" alt="" />
                         <span>Private</span>
                     </div>
@@ -27,7 +40,7 @@ const AddRoomModel = () => {
             </div>
             <div className={styles.modelFooter}>
                 <h3>Start a room, open to everyone</h3>
-                <button className={styles.footerButton}><img src="/images/party.png" alt="" /><span>Let's Go!</span></button>
+                <button onClick={createRoom} className={styles.footerButton}><img src="/images/party.png" alt="" /><span>Let's Go!</span></button>
             </div>
         </div>
     </div>
