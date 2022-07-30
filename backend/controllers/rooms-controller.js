7fdde8f5f2fa7856c66c493338de39a09039aee2
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2022-07-30 22:48:06 +0530

    add index method

--- backend/controllers/rooms-controller.js
+++ backend/controllers/rooms-controller.js
@@ -13,6 +13,11 @@ class RoomsController{
         });
         return res.json(new RoomDto(room))
     }
+    async index(req,res){
+        const rooms = await roomService.getAllRooms(['open']);
+        const allRooms = rooms.map(room=>new RoomDto(room));
+        return res.json(allRooms)
+    }
 
 }
 
