dc31ffd66b1dd794255ea906370ed61c0cc3b518
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2022-08-02 22:14:31 +0530

    rooms to render all the speakers

--- frontend/src/pages/Room/Room.jsx
+++ frontend/src/pages/Room/Room.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React,{useState} from 'react'
+import { useWebRTC } from '../../hooks/useWebRTC'
 
 const Room = () => {
+    const {clients} = useWebRTC();
   return (
-    <div>Room</div>
+    <div>
+        <div>All connected speakers</div>
+        {
+            clients.map(client=>{
+                return (
+                <div key={client.id}>
+                    <audio controls autoPlay></audio>
+                    {
+                        client.name
+                    }
+                </div>)
+            })
+        }
+    </div>
   )
 }
 
