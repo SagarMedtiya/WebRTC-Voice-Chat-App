b7c68d3576dfdb348382afb0c469c9e8171c4f7d
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2022-08-04 23:02:11 +0530

    implement provideref to get the each client speake

--- frontend/src/hooks/useWebRTC.js
+++ frontend/src/hooks/useWebRTC.js
@@ -1,15 +1,41 @@
-import {useState} from 'react'
-import { useStateWithCallBack } from './useStateWithCallBack'
-export const useWebRTC=()=>{
-    const [clients, setClients] = useStateWithCallBack([
-        {
-            id: 1,
-            name: 'Sagar'
-        },
-        {
-            id: 2,
-            name: 'abhishek'
+import { useCallback, useEffect } from 'react';
+import {useRef} from 'react'
+import { useStateWithCallBack } from './useStateWithCallBack';
+
+export const useWebRTC=(roomId, user)=>{
+    const [clients, setClients] = useStateWithCallBack([]);
+    const audioElements = useRef({});
+    const connections = useRef({});
+    const localMediaStream = useRef(null);
+    const provideRef=(instance, userId)=>{
+        audioElements.current[userId] = instance; 
+    }
+    const addNewClients = useCallback(
+        (newClients, cb)=>{
+            const lookingFor = clients.find((client)=>client.id === newClients.id)
+            if(lookingFor=== undefined){
+                setClients((existingClients)=>[...existingClients, newClients], cb)
+            }
+        },[clients, setClients]
+    )
+    //capture media
+    useEffect(() => {
+        const startCapture=async ()=>{
+            localMediaStream.current = 
+                await navigator.mediaDevices.getUserMedia({
+                    audio:true
+                })
         }
-    ])
-    return {clients}
+        startCapture().then(()=>{
+            addNewClients(user,()=>{
+                const localElement = audioElements.current[user.id];
+                if(localElement){
+                    //localElement.volume = 0;
+                    localElement.srcObject = localMediaStream.current;
+                }
+            })
+        })
+    }, []);
+
+    return {clients, provideRef}
 }
