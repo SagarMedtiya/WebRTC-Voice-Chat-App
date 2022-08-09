69994c6c03732b385e8a33b2b7f9c109409867f8
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2022-08-09 23:54:25 +0530

    add relay_sdp

--- backend/action.js
+++ backend/action.js
@@ -2,7 +2,8 @@ const ACTIONS ={
     JOIN: 'join',
     LEAVE: 'leave',
     ADD_PEER: 'add-peer',
-    RELAY_ICE: 'relay_ice'
+    RELAY_ICE: 'relay_ice',
+    RELAY_SDP: 'relay_sdp'
 }
 
 module.exports = ACTIONS;
