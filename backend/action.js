fe118e60d23c7826cfd928a90146481c44929fd7
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2022-08-10 20:34:46 +0530

    add sdp and ice_candidate

--- backend/action.js
+++ backend/action.js
@@ -3,7 +3,9 @@ const ACTIONS ={
     LEAVE: 'leave',
     ADD_PEER: 'add-peer',
     RELAY_ICE: 'relay_ice',
-    RELAY_SDP: 'relay_sdp'
+    RELAY_SDP: 'relay_sdp',
+    ICE_CANDIDATE: 'ice-candidate',
+    SESSION_DESCRIPTION: 'session-description'
 }
 
 module.exports = ACTIONS;
