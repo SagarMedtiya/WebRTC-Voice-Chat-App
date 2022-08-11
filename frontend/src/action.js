11c728c283265865dae98f98543ac564567a7104
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2022-08-11 17:10:36 +0530

    add session_description and remove_peer

--- frontend/src/action.js
+++ frontend/src/action.js
@@ -5,5 +5,6 @@ export const ACTIONS ={
     RELAY_ICE: 'relay_ice',
     RELAY_SDP: 'relay_sdp',
     ICE_CANDIDATE: 'ice-candidate',
-    SESSION_DESCRIPTION: 'session-description'
+    SESSION_DESCRIPTION: 'session-description',
+    REMOVE_PEER: 'remove-peer'
 }
