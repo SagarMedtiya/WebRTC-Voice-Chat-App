2a1ef32da409d8ba39bb16c4e133485d3b73c87c
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2022-08-13 23:00:05 +0530

    fix the bugs

--- frontend/src/action.js
+++ frontend/src/action.js
@@ -2,8 +2,8 @@ export const ACTIONS ={
     JOIN: 'join',
     LEAVE: 'leave',
     ADD_PEER: 'add-peer',
-    RELAY_ICE: 'relay_ice',
-    RELAY_SDP: 'relay_sdp',
+    RELAY_ICE: 'relay-ice',
+    RELAY_SDP: 'relay-sdp', 
     ICE_CANDIDATE: 'ice-candidate',
     SESSION_DESCRIPTION: 'session-description',
     REMOVE_PEER: 'remove-peer'
