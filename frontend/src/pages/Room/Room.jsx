1e967091c9f734dc0bf38aa7ff7350dc3747be7e
Sagar Medtiya <41753173+SagarMedtiya@users.noreply.github.com>
2022-08-16 17:24:14 +0530

    implement the css

--- frontend/src/pages/Room/Room.jsx
+++ frontend/src/pages/Room/Room.jsx
@@ -2,8 +2,10 @@ import React,{useState} from 'react'
 import { useWebRTC } from '../../hooks/useWebRTC'
 import {useParams } from 'react-router-dom'
 import { useSelector } from 'react-redux';
+import styles from './Room.module.css'
 const Room = () => {
     const {id: roomId} =  useParams();
+    console.log(roomId);
     const user = useSelector(state => state.auth.user)
     const {clients, provideRef} = useWebRTC(roomId,user);
   return (
@@ -12,8 +14,9 @@ const Room = () => {
         {
             clients.map((client,index)=>{
                 return (
-                <div key={index}>
-                    <audio ref={(instance)=>provideRef(instance,client.id)}controls autoPlay></audio>
+                <div className={styles.userHead}key={index}>
+                    <audio ref={(instance)=>provideRef(instance,client.id)} controls autoPlay></audio>
+                    <img className={styles.userAvatar} src={client.avatar} alt="" />
                     {
                         client.name
                     }
